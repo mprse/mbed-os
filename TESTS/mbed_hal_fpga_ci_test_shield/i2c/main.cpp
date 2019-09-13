97c38423d542d34c5cfc27cde015719704c6d36a
Przemyslaw Stekiel <przemyslaw.stekiel@mobica.com>
2019-09-13 08:29:48 +0200

    Adapt FPGA tests for spi, i2c, analogin, pwm, uart to test direct init (explicit pinmap).

--- TESTS/mbed_hal_fpga_ci_test_shield/i2c/main.cpp
+++ TESTS/mbed_hal_fpga_ci_test_shield/i2c/main.cpp
@@ -31,9 +31,9 @@
 #include "mbed.h"
 #include "i2c_api.h"
 #include "pinmap.h"
+#include "hal/explicit_pinmap.h"
 #include "test_utils.h"
 #include "I2CTester.h"
-
 
 using namespace utest::v1;
 
@@ -53,6 +53,7 @@ void test_i2c_init_free(PinName sda, PinName scl)
     i2c_frequency(&obj, 100000);
 }
 
+template<bool init_direct>
 void i2c_test_write(PinName sda, PinName scl)
 {
     // Remap pins for test
@@ -66,7 +67,17 @@ void i2c_test_write(PinName sda, PinName scl)
     // Initialize mbed I2C pins
     i2c_t i2c;
     memset(&i2c, 0, sizeof(i2c));
-    i2c_init(&i2c, sda, scl);
+    if (init_direct) {
+#if EXPLICIT_PINMAP_READY
+        const i2c_pinmap_t pinmap = get_i2c_pinmap(sda, scl);
+        i2c_init_direct(&i2c, &pinmap);
+#else
+        //skip this test case if explicit pinmap is not supported
+        return;
+#endif
+    } else {
+        i2c_init(&i2c, sda, scl);
+    }
     i2c_frequency(&i2c, 100000);
 
     // Reset tester stats and select I2C
@@ -140,6 +151,7 @@ void i2c_test_write(PinName sda, PinName scl)
     tester.pin_set_pull(scl, MbedTester::PullNone);
 }
 
+template<bool init_direct>
 void i2c_test_read(PinName sda, PinName scl)
 {
     // Remap pins for test
@@ -153,7 +165,12 @@ void i2c_test_read(PinName sda, PinName scl)
     // Initialize mbed I2C pins
     i2c_t i2c;
     memset(&i2c, 0, sizeof(i2c));
-    i2c_init(&i2c, sda, scl);
+    if (init_direct) {
+        const i2c_pinmap_t pinmap = get_i2c_pinmap(sda, scl);
+        i2c_init_direct(&i2c, &pinmap);
+    } else {
+        i2c_init(&i2c, sda, scl);
+    }
     i2c_frequency(&i2c, 100000);
 
     // Reset tester stats and select I2C
@@ -425,8 +442,10 @@ void i2c_test_byte_read(PinName sda, PinName scl)
 
 Case cases[] = {
     Case("i2c - init/free test all pins", all_ports<I2CPort, DefaultFormFactor, test_i2c_init_free>),
-    Case("i2c - test write i2c API", all_peripherals<I2CPort, DefaultFormFactor, i2c_test_write>),
-    Case("i2c - test read i2c API", all_peripherals<I2CPort, DefaultFormFactor, i2c_test_read>),
+    Case("i2c - test write i2c API", all_peripherals<I2CPort, DefaultFormFactor, i2c_test_write<false>>),
+    Case("i2c (direct init) - test write i2c API", all_peripherals<I2CPort, DefaultFormFactor, i2c_test_write<true>>),
+    Case("i2c - test read i2c API", all_peripherals<I2CPort, DefaultFormFactor, i2c_test_read<false>>),
+    Case("i2c (direct init) - test read i2c API", all_peripherals<I2CPort, DefaultFormFactor, i2c_test_read<true>>),
     Case("i2c - test single byte write i2c API", all_peripherals<I2CPort, DefaultFormFactor, i2c_test_byte_write>),
     Case("i2c - test single byte read i2c API", all_peripherals<I2CPort, DefaultFormFactor, i2c_test_byte_read>)
 };
